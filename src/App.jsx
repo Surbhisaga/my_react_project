ca7107290afc299f66400146cc279f6eedea1e8d
surbhi sagathiya <surbhis.mobio@gmail.com>
2021-07-08 12:27:23 +0530

    multiple component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Heading';
+import Heading from './Heading';
+import Para from './Para';
+import OrderData from './OrderData'
+
+function App(){
+    return(
+        <>
+            <Heading/>
+            <Para/>
+            <OrderData/>
+        </>
+    );
+}
+
+export default App;
---
+++ src/OrderData.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function OrderData(){
+    return(
+    <ol>
+        <li>surbhi</li>
+        <li>radha</li>
+        <li>harsh</li>
+        <li>krish</li>
+        <li>vinay</li>
+    </ol>); 
+}
+
+export default OrderData;
