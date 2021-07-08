06240f0c32e1a27e4777c2cbdba175bc67d1d9b9
surbhi sagathiya <surbhis.mobio@gmail.com>
2021-07-08 12:42:40 +0530

    greeting component

--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App'
 
-let curDate=new Date(2020,9,5,2);
-curDate = curDate.getHours();
-let greeting = '';
-const cssStyle={};
 
-if(curDate>=1 && curDate<12){
-  greeting="Good Morning";
-  cssStyle.color="green";
-}else if(curDate>=12 && curDate<19){
-  greeting="Good Afternoon";
-  cssStyle.color="orange";
-}else{
-  greeting="Good Night";
-  cssStyle.color="black";
-}
-
-ReactDOM.render(<>
-    <div>
-      <h1>Hello sir , <span style={cssStyle}>{greeting}</span></h1>
-    </div>
-    </>,
-  document.getElementById('root'));
+ReactDOM.render(<App/>,document.getElementById('root'));
